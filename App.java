bf5effb689c06cedd2c37cd86d4f76f079952b08
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-06 20:36:35 +0500

    submitt

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Muhammad Hasnain");
+        System.out.println("I am a student of BSE");
+        System.out.println("Now i am study in comsats university");
+    }
+}
