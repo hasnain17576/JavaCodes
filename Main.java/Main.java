1736ee3c43be0426281be0f9c747a3dfad92cd20
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-06 20:57:38 +0500

    submitt

---
+++ Main.java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(23);
+        System.out.println(5435);
+        System.out.println(8705743);
+        System.out.println(432953553);
+    }
+}
