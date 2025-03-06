de5cbe77f088d6f3dcce3d9ff20619a0909c4258
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-06 21:37:26 +0500

    submitt

--- Main.java/src/App.java
+++ Main.java/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(3+4);
-        System.out.println(4*5);
+        String name = "hasnain";
+        System.out.println(name);
     }
 }
