b47087f29aea64131c76a878ead796ae4947ec3e
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-11 15:23:56 +0500

    java

---
+++ zaawar wasli/src/App1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a/2==0) {
+            System.out.println("even");
+        } else{
+            System.out.println("odd");
+
+
+        }
+            
+        }
+    }
+
