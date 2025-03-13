d8ccd0c61b3f851f89c2ceb695e6fe367f9ff232
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-13 18:08:44 +0500

    submitt

---
+++ java1/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int counter = 0; counter<30; counter = counter+1){
+            System.out.println("hasnainwasli");
+
+
+        }
+      
+    }
+}
--- zaawar wasli/src/App4.java
+++ zaawar wasli/src/App4.java
@@ -1,4 +1,4 @@
-public class App3 {
+public class App4 {
     public static void main(String[] args) {
         for(int counter = 0; counter >30; counter=counter+1){
             System.out.println("hasnain");
