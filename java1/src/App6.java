58683dd1dce7e5031880635c4e9efbea7df5a0db
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-15 21:44:03 +0500

    submitt

--- java1/src/App6.java
+++ java1/src/App6.java
@@ -1,12 +1,17 @@
-public class App5{
+public class App6{
     public static void main(String[] args) {
         int n = 4;
         
-        for( int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-             System.out.println(); 
+             for( int i=n;i<=1;i++){
+                for(int j=1;j<=n-1;j++){
+                    System.out.print(" ");
+                }
+                 for(int j=1; j<=i;j++){
+                    System.out.print("*");
+                }
+                 
+                System.out.println();
         }
-    }        
+    }           
 }
+ 
