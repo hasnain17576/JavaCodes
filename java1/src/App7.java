ba5b43be3f3d96f9bf25c8560e5100023191c30e
hasnain17576 <hasnainwasli17@gmail.com>
2025-03-18 20:49:16 +0500

    submitt

--- java1/src/App7.java
+++ java1/src/App7.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class App7{
     public static void main(String[] args) {
-        int n = 8;
-        
-        for( int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
-               
-
-            }
-            System.out.println();  
+        Scanner sc = new Scanner(System.in);
+      /*   int a = sc.nextInt();
+        System.out.println(); */
+        System.out.println("enter the your name ");
+        String a = sc.next();
+        System.out.println("enter your weight ");
+        int b = sc.nextInt();
+        System.out.println("Enter your age");
+        int c = sc.nextInt();
+        System.out.println("Enter your cgpa");
+        int d = sc.nextInt();
+        System.out.println("This your name="+a);
+        System.out.println("This is your weight="+b);
+        System.out.println("This is your age="+c );
+        System.out.println("this is your cgpa="+d);
+        sc.close();
         }
     }           
-}
  
---
+++ java1/src/App8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App8{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius  = sc.nextDouble();
+        System.out.println("Enter your radius of the circle");
+        double area= Math.PI*radius*radius;
+        System.out.println("enter your circle of the circle="+area+"cm ");
+        sc.close();
+        
+                
+
+  
+        }
+    }           
+ 
