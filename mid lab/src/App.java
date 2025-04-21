ca162eef7d39bb86ef071778a75a614a80d36b2e
hasnain17576 <hasnainwasli17@gmail.com>
2025-04-21 13:30:35 +0500

    submit

---
+++ mid lab/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        int unit= sc.nextInt();
+        if(unit>=50){
+            unit 1 = unit*4;          
+        }else if(unit>=300){
+            unit 2 = (unit*15)-unit 1;
+        }else if(unit>=400){
+            unit 3 = (unit*20)-unit 2;
+            
+        }else(unit>750){
+            unit 4= (unit*30)-unit 3;
+        }
+        System.out.println(total bill);
+
+        
+    }
+}
