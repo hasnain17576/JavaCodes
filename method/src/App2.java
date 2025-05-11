ebfeef9327317c9fad37af22c353cacb12475710
hasnain17576 <hasnainwasli17@gmail.com>
2025-05-11 19:25:42 +0500

    submitt

---
+++ method/src/App2.java
@@ -0,0 +1,12 @@
+
+public class App2{
+    public static void printMessage() {
+        System.out.println("Welcome to Java Programming!");
+      }
+    
+      public static void main(String[] args) {
+        printMessage();
+      }
+    }
+
+  
