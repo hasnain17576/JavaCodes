e9e282199735a9a9af44353d5e8f230e69fa071c
hasnain17576 <hasnainwasli17@gmail.com>
2025-05-13 15:15:06 +0500

    submitt

---
+++ method/src/App5.java
@@ -0,0 +1,13 @@
+
+public class App5{ 
+        public static void main(String[] args){ 
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println("Before invoking the swap method,num1 is  "+num1+"and num2 is"+num2);
+        swap(num1,num2);
+        System.out.println("After invoking the swap method,num1 is  "+num1+"and num2 is"+num2);
+        }
+        
+
+        
+  
