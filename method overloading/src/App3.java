6d7e2dfbc72125a15ba134ac616dfacf102baa97
hasnain17576 <hasnainwasli17@gmail.com>
2025-05-24 15:14:54 +0500

    submitt

--- method overloading/src/App3.java
+++ method overloading/src/App3.java
@@ -1,18 +1,20 @@
 public class App3 {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-    public static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-    public static double add(double a, double b) {
-        return a + b;
-    }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
+    int i = 5;
+    int j = 2;
+    int k = max(i, j); // function call
 
-        System.out.println(add(5, 10));        
-        System.out.println(add(1, 2, 3));      
-        System.out.println(add(2.5, 3.5));      
-    }
+    System.out.println("The maximum between " + i + " and " + j + " is " + k);
+}
+
+public static int max(int num1, int num2) {
+    int result;
+    if (num1 > num2)
+        result = num1;
+    else
+        result = num2;
+
+    return result;
+}
 }
