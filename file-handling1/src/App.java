44223adaf5634f6d4d0f83914699175457cbcc36
hasnain17576 <hasnainwasli17@gmail.com>
2025-07-01 21:15:35 +0500

    submitt

---
+++ file-handling1/src/App.java
@@ -0,0 +1,22 @@
+import java.io.File;
+ import java.io.PrintWriter;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        File file=new File("scores.txt");
+        if(file.exists()){
+            System.out.println("File already exists");
+            System.exit(0);
+        }
+        try(PrintWriter output=new PrintWriter(file);){
+        
+
+        output.print("muhammad hasnain");
+        output.println(45);
+        output.print("arhamali bhatti");
+        output.println(23);
+        output.close();
+    }
+}
+}
+
